f85e4a62ab07adbda5d6695636a88e648df5ef16
jiao <jql132993977@sina.com>
2016-04-20 10:10:54 +0800

    初始化项目

---
+++ HM2/HM2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HM2
+//
+//  Created by JqlLove on 16/4/20.
+//  Copyright © 2016年 JqlLove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
